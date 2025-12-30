43f1162ea7b3dc7faaad44963c3b7c8092871bc4
aichabcm-lab <aichabcm@gmail.com>
2025-12-30 23:46:21 +0100

    ajout

--- src/Security/AuthAuthenticator.php
+++ src/Security/AuthAuthenticator.php
@@ -2,17 +2,19 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class AuthAuthenticator extends AbstractLoginFormAuthenticator
@@ -21,21 +23,22 @@ class AuthAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
-    }
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private EntityManagerInterface $entityManager
+    ) {}
 
     public function authenticate(Request $request): Passport
     {
-        $email = (string) $request->request->get('email', '');
-
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+        $email = $request->request->get('email', '');
+        $password = $request->request->get('password', '');
 
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials((string) $request->request->get('password', '')),
+            new PasswordCredentials($password),
             [
-                new CsrfTokenBadge('authenticate', (string) $request->request->get('_csrf_token')),            ]
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
+            ]
         );
     }
 
@@ -45,9 +48,8 @@ class AuthAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirige vers la page d'accueil
+        return new RedirectResponse('/');
     }
 
     protected function getLoginUrl(Request $request): string
