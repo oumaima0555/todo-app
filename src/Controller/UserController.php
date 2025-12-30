82e909fb2789532f072076ed6623a45a55a586bc
aichabcm-lab <aichabcm@gmail.com>
2025-12-30 23:53:13 +0100

    ajout

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        // Redirige vers l'accueil
+        return $this->redirectToRoute('/');
+    }
+}
